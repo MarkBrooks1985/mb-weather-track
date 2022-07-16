4509e41bf1159b1dcf1caf444717da12835596c9
Alynn'nari Alish'sundi <alynnnari@gmail.com>
2022-07-16 18:29:40 +0930

    fourth commit, script is mostly done, need to finish it and finish the UI

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var searchBtn = document.querySelector("#searchBtn")
+var weatherDashboard = document.getElementById("weather-details")
 
 searchBtn.addEventListener('click', searchCity)
 
@@ -26,6 +27,7 @@ function fetchLocation(searchCity) {
             console.log(data[0])
             var lat = data[0].lat;
             var lon =  data[0].lon;
+            fetchWeather(lat, lon, searchCity);
             console.log(lat, lon)
             // $("textarea#ExampleMessage").val(result.exampleMessage);
            // var result = fetchWeather(lat, lon);
@@ -37,28 +39,76 @@ function fetchLocation(searchCity) {
     })
 }
 
-function fetchWeather(lat, lon) {
-    fetch(`https://api.openweathermap.org//geo/1.0/direct?q=${searchCity}&limit=5&appid=d63d983b773a00bdb15f82fa99ec7c45`)
+function fetchWeather(lat, lon, cityName) {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=d63d983b773a00bdb15f82fa99ec7c45`)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
-        if(!data[0]){
-            alert("Location is not exist for the city")
-        }
-        else {
-
-        }
+        renderResult(data, cityName);
+        renderForecast(data);
     })
     .catch(function(error){
         console.error("Error featching data", error)
     })
 }
 
-function renderResult(result){
-   
+function renderResult(result, cityName){
+    // show date next to city name
+    // create element > add text > add the element to weatherDashboard
+    var h2El = document.createElement('h2')
+    var iconEl = document.createElement("img")
+    iconEl.src = "http://openweathermap.org/img/wn/"+ result.current.weather[0].icon +".png"
+    h2El.textContent = cityName + " " + moment.unix(result.current.dt).format("DD/MM/YYYY")
+    h2El.appendChild(iconEl);
+    weatherDashboard.appendChild(h2El);
+
+    // icon
+    // extracted from result temp, wind, humidity, uv index
+    var tempEl = document.createElement('p')
+    tempEl.textContent = "Temp: " + result.current.temp
+    weatherDashboard.appendChild(tempEl);
+
+    var windEl = document.createElement('p')
+    windEl.textContent = "Wind: " + result.current.wind_speed
+    weatherDashboard.appendChild(windEl);
+
+    var humidEl = document.createElement('p')
+    humidEl.textContent = "Humidity: " + result.current.humidity
+    weatherDashboard.appendChild(humidEl);
+
+    var uviEl = document.createElement('p')
+    uviEl.textContent = "UV Index: " + result.current.uvi
+    weatherDashboard.appendChild(uviEl);
 }
 
+function renderForecast(result) {
+ // result.daily - array - use loop starting 0 till <5
+ for (let index = 0; index < 5; index++) {
+    // create 
+    // add text value
+    // 
+    var h2El = document.createElement('h2')
+    var iconEl = document.createElement("img")
+    iconEl.src = "http://openweathermap.org/img/wn/"+ result.daily.weather[0].icon +".png"
+    h2El.textContent = moment.unix(result.daily.dt).format("DD/MM/YYYY")
+    h2El.appendChild(iconEl);
+    forecast.appendChild(h2El);
+
+    var tempEl = document.createElement('p')
+    tempEl.textContent = "Temp: " + result.daily.temp
+    forecast.appendChild(tempEl);
+
+    var windEl = document.createElement('p')
+    windEl.textContent = "Wind: " + result.current.wind_speed
+    forecast.appendChild(windEl);
+
+    var humidEl = document.createElement('p')
+    humidEl.textContent = "Humidity: " + result.current.humidity
+    forecast.appendChild(humidEl);
+    // append to forecast
+ };
+};
 // https://api.openweathermap.org//geo/1.0/direct?q=Adelaide&limit=5&appid=d63d983b773a00bdb15f82fa99ec7c45
 
 console.log(searchBtn);
